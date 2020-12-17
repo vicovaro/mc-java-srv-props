959d3c7cc0938f1792302c63e647277adfcf8509
Tai Chen Li <dev@taichen.li>
2020-12-17 02:36:35 -0500

    added a sidenav and a toolbar as an early version

--- src/app/components/forms-page/forms-page.component.ts
+++ src/app/components/forms-page/forms-page.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+
+export const DEFAULT_HEIGHT = 100;
+export const DEFAULT_DRAWER_WIDTH = 300;
 
 @Component({
     selector: 'app-forms-page',
     templateUrl: './forms-page.component.html',
     styleUrls: ['./forms-page.component.scss'],
 })
-export class FormsPageComponent implements OnInit {
-    constructor() {}
+export class FormsPageComponent implements AfterContentInit {
+    mainContainerHeight = DEFAULT_HEIGHT;
+    drawerWidth = DEFAULT_DRAWER_WIDTH;
 
-    ngOnInit() {}
+    ngAfterContentInit() {
+        this.mainContainerHeight = window.innerHeight;
+        this.drawerWidth = window.innerWidth * 0.75;
+    }
 }
