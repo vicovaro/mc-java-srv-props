3032f07083246d8ea9ebddb276e07144f212f2a7
Tai Chen Li <dev@taichen.li>
2020-12-26 19:25:35 -0500

    added more configs to the server and structures to generate mat-card for the forms

--- src/app/components/stepper-content/stepper-content.component.ts
+++ src/app/components/stepper-content/stepper-content.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { default as ServerProperties } from '@assets/server-properties.json';
+import { Observable } from 'rxjs';
+
+export interface AutocompleteForms {
+    value: string;
+    options: Array<string>;
+    filteredOptions: Observable<string[]>;
+}
+export interface SelectedForms {
+    value: string;
+    options: Array<string>;
+}
+export interface NumberRangeForms {
+    value: number;
+    min: number;
+    max: number;
+}
 @Component({
     selector: 'app-stepper-content',
     templateUrl: './stepper-content.component.html',
     styleUrls: ['./stepper-content.component.scss'],
 })
 export class StepperContentComponent implements OnInit {
-    formsServerProperties: FormControl[] = [];
+    checkBoxForms: Map<string, boolean> = new Map();
+    regexForms: Map<string, FormControl> = new Map();
+    autocompleteForms: Map<string, AutocompleteForms> = new Map();
+    selectorForms: Map<string, SelectedForms> = new Map();
+    numberRange: Map<string, NumberRangeForms > = new Map();
 
-    private readonly propertyNameIndex: number = 0;
-    private readonly propertyTypeIndex: number = 1;
-    private serverProperties: [string, unknown][] = []; // parsed JSON
+    constructor() {}
 
     ngOnInit() {
-        this.serverProperties = Object.entries(ServerProperties);
+        const serverProperties = Object.entries(ServerProperties);
+        console.log(serverProperties);
+        serverProperties.forEach(property => {
+            console.log(property);
+        });
     }
 }
